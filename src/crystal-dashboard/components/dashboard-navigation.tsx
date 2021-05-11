29112c0a74a11f4cd0e6f812e20eeb6294a15f4d
Thomas shih <tt7919@gmail.com>
2021-05-11 22:04:21 +0800

    feat: logout function

--- src/crystal-dashboard/components/dashboard-navigation.tsx
+++ src/crystal-dashboard/components/dashboard-navigation.tsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { MainPath } from 'src/core/enums/main-path';
 import Navigation from 'src/core/models/navigation';
 
-export default function DashboardNavigation(props: { navigation: Navigation[] }) {
-  const { navigation } = props;
+export default function DashboardNavigation(props: { navigation: Navigation[]; currentUrl: string }) {
+  const { navigation, currentUrl } = props;
+  const history = useHistory();
+  const signOut = () => {
+    history.push(MainPath.EmployeeLogin);
+  };
+
   return (
-    <div className="bg-blue-50 h-full border-r border-gray-300 p-5 space-y-4" style={{ flex: '0 0 250px' }}>
-      {navigation.map((nav) => (
-        <Link className="flex justify-between" to={nav.path}>
-          <label className="text-xl">{nav.text}</label>
-          <span> {'>'} </span>
-        </Link>
-      ))}
+    <div className="flex flex-col bg-blue-50 h-full border-r border-gray-300 p-5" style={{ flex: '0 0 250px' }}>
+      <div className="flex-1 space-y-4">
+        {navigation.map((nav) => (
+          <Link key={nav.key} className="flex justify-between" to={`${currentUrl}${nav.path}`}>
+            <label className="text-xl">{nav.text}</label>
+            <span> {'>'} </span>
+          </Link>
+        ))}
+      </div>
+      <button onClick={signOut}>登出</button>
     </div>
   );
 }
--- src/crystal-dashboard/index.tsx
+++ src/crystal-dashboard/index.tsx
@@ -1,39 +1,45 @@
 import DashboardNavigation from 'src/crystal-dashboard/components/dashboard-navigation';
 import HandSizeForm from 'src/crystal-dashboard/components/handsize-form';
 import SliverPipeForm from 'src/crystal-dashboard/components/sliver-pipe-form';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import { MainPath } from 'src/core/enums/main-path';
 import { DASHBOARD_NAVIGATION } from 'src/core/constants/constants';
 import UploadImageForm from 'src/crystal-dashboard/components/upload-image-form';
 import AllowListController from 'src/crystal-dashboard/components/allowlist-control';
 
 export default function CrystalDashboard() {
+  const { url } = useRouteMatch();
+
   return (
     <div className="h-full flex">
-      <Router>
-        <DashboardNavigation navigation={DASHBOARD_NAVIGATION} />
-        <div className="flex flex-col flex-grow items-center divide-y-2">
-          <div className="w-full">
-            <Switch>
-              <Route path={MainPath.AllowList}>
-                <AllowListController />
-              </Route>
-              <Route path={MainPath.HandSizeForm}>
-                <HandSizeForm />
-              </Route>
-              <Route path={MainPath.SliverPipeForm}>
-                <SliverPipeForm />
-              </Route>
-              <Route path={MainPath.CrystalBeadForm}>
-                <UploadImageForm tableName="crystalImages" />
-              </Route>
-              <Route path={MainPath.FlowerCoverForm}>
-                <UploadImageForm tableName="flowerCovers" />
-              </Route>
-            </Switch>
-          </div>
+      <DashboardNavigation navigation={DASHBOARD_NAVIGATION} currentUrl={url} />
+      <div className="flex flex-col flex-grow items-center divide-y-2">
+        <div className="w-full">
+          <Switch>
+            <Route path={`${url}${MainPath.AllowList}`}>
+              <AllowListController />
+            </Route>
+            <Route path={`${url}${MainPath.HandSizeForm}`}>
+              <HandSizeForm />
+            </Route>
+            <Route path={`${url}${MainPath.SliverPipeForm}`}>
+              <SliverPipeForm />
+            </Route>
+            <Route path={`${url}${MainPath.CrystalBeadForm}`}>
+              <UploadImageForm tableName="crystalImages" />
+            </Route>
+            <Route path={`${url}${MainPath.FlowerCoverForm}`}>
+              <UploadImageForm tableName="flowerCovers" />
+            </Route>
+            <Route path={`${url}${MainPath.FlowerCoverForm}`}>
+              <UploadImageForm tableName="flowerCovers" />
+            </Route>
+            <Route exact path={`${url}`}>
+              <Redirect to={`${url}${MainPath.AllowList}`}></Redirect>
+            </Route>
+          </Switch>
         </div>
-      </Router>
+      </div>
     </div>
   );
 }
